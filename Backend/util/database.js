ae2d156d023e5916abadb53273bcc59ab2af26a6
Sourav0809 <spathak7431@gmail.com>
2024-03-05 01:14:58 +0530

    vercel deploy fix

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize')
 
 
 const sequelize = new Sequelize('redbubble', process.env.DB_USERNAME, process.env.DB_PWD, {
-    dialect: process.env.DIALECT,
+    dialect: "mysql",
     host: process.env.DB_HOST,
     logging: false
 })
