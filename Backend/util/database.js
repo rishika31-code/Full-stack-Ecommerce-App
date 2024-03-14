2c41d523284df1fdf5d0cfffda21f8596d073387
Sourav0809 <spathak7431@gmail.com>
2024-03-15 01:34:48 +0530

    deployment fix

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -5,12 +5,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     dialect: process.env.DIALECT,
     host: process.env.DB_HOST,
     logging: false,
-    // dialectOptions: {
-    //     ssl: {
-    //         require: true,
-    //         rejectUnauthorized: false
-    //     }
-    // }
+    dialectOptions: process.env.DB_SSL_OPTIONS
 })
 
 
