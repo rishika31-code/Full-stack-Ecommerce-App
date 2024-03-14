9c10b2d9814fcfe2c5f54e3222a7a6dac45f87e4
Sourav0809 <spathak7431@gmail.com>
2024-03-14 23:52:30 +0530

    deployment fix

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -4,7 +4,8 @@ const { Sequelize } = require('sequelize')
 const sequelize = new Sequelize('redbubble', process.env.DB_USERNAME, process.env.DB_PWD, {
     dialect: process.env.DIALECT,
     host: process.env.DB_HOST,
-    logging: false
+    logging: false,
+    ssl: true,
 })
 
 
