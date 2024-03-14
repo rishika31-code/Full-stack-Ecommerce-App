6f572cbeb41bad72458f40656781684e42f7b451
Sourav0809 <spathak7431@gmail.com>
2024-03-15 01:42:43 +0530

    fix

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, pr
     dialect: process.env.DIALECT,
     host: process.env.DB_HOST,
     logging: false,
-    dialectOptions: process.env.DB_SSL_OPTIONS
+    dialectOptions: process.env.DB_SSL_OPTIONS && JSON.parse(process.env.DB_SSL_OPTIONS)
 })
 
 
