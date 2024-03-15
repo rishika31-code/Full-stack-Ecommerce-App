e2b66fc457b9e3662e56032808e9353e8522f366
Sourav0809 <spathak7431@gmail.com>
2024-03-15 14:08:19 +0530

    deployement bug fix pg added on db config

--- Backend/util/database.js
+++ Backend/util/database.js
@@ -1,10 +1,11 @@
 const { Sequelize } = require('sequelize')
-
+const pg = require('pg')
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PWD, {
     dialect: process.env.DIALECT,
     host: process.env.DB_HOST,
     logging: false,
+    dialectModule: pg,
     dialectOptions: {
         ssl: {
             require: true,
