d047c4d4e229b7d1a453502c68e75c85a66fb1c9
Sourav0809 <spathak7431@gmail.com>
2024-03-21 02:25:51 +0530

    cors configured

--- Backend/index.js
+++ Backend/index.js
@@ -15,8 +15,20 @@ const bodyParser = require('body-parser')
 const db = require('./util/database')
 const app = express()
 
+// Configure CORS with options
+const corsOptions = {
+    origin: 'https://adminredbuuble.vercel.app/',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 200
+};
+
+
+
 // Applying middlewares
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
